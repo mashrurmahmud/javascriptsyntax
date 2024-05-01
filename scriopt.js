c284209f35218a6d51a39c23833675f0b2dd6370
werehouse <mahmudmashrur0@gmail.com>
2024-05-01 23:16:30 +0600

    first transaction

---
+++ scriopt.js
@@ -0,0 +1,38 @@
+
+//Ans no 2
+
+var firstnumber;
+
+var secondnumber;
+var thirdnumber ;
+
+firstnumber = 10;
+secondnumber = 20;
+thirdnumber = 30;
+
+console.log(firstnumber,secondnumber,thirdnumber);
+
+
+//Ans no 3
+
+//let can be declared one time,but if you redeclare the let again with same variable, the program gives you error
+// var can be declared many times;
+// const can be declared one time,but you cann't change the value of data type.
+
+
+// Ans no 4
+console.log("hello world");
+
+
+
+// Ans no 5
+
+alert("Hello world");
+
+//Ans no 6
+
+let firstnumber = 10;
+ let secondnumber =20;
+
+sum = firstnumber + secondnumber;
+console.log(sum);
